057062dbd8fb2b4030a56ef415c25d2f18b73cc3
Elodie B <billelodie@gmail.com>
2023-03-01 16:43:20 +0100

    carrousel non fonctionnel

--- src/pages/Fiche_logmement/FicheLog.jsx
+++ src/pages/Fiche_logmement/FicheLog.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Caroussel from "../../components/Fiche_logement/Caroussel";
+import Carrousel from "../../components/Fiche_logement/Caroussel";
 import { useParams } from "react-router-dom";
 import data from "../../data/data.json"
 
@@ -10,20 +10,17 @@ function FicheLogement() {
 
     const findLogement = data.find((logement) => {
         return logement.id === id;
-
     })
+    console.log(findLogement);
 
-    console.log(findLogement.pictures)
 
-    const pics = findLogement.pictures;
     return (
         <React.Fragment>
 
-            <Caroussel srcImg={pics} />
+            <Carrousel />
 
 
         </React.Fragment>
     )
 }
-
 export default FicheLogement;
